5d0d2e9713528be6edb5bcf3b3ecbaecfbd8575c
Jessica242424 <Jessica242424@localhost>
2014-09-25 00:04:54 -0700

    finished the create statements

--- src/com/statboost/db/clean.sql
+++ src/com/statboost/db/clean.sql
@@ -25,13 +25,14 @@ CREATE TABLE `stt_magic_card` (
   `mcr_border` varchar(15) DEFAULT NULL,
   `mcr_hand` int(11) DEFAULT NULL,
   `mcr_life` int(11) DEFAULT NULL,
-  `mcr_timeshifted` boolean DEFAULT FALSE,
-  `mcr_reserved` boolean DEFAULT FALSE,
+  `mcr_timeshifted` tinyint(1) DEFAULT '0',
+  `mcr_reserved` tinyint(1) DEFAULT '0',
   `mcr_release_date` varchar(50) DEFAULT NULL,
   `mcr_uid` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`mcr_uid`),
+  KEY `stt_magic_card` (`mcr_set_id`),
   CONSTRAINT `stt_magic_card` FOREIGN KEY (`mcr_set_id`) REFERENCES `stt_magic_set` (`mst_uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25952 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `stt_magic_set` (
   `mst_name` varchar(100) DEFAULT NULL,
@@ -44,7 +45,6 @@ CREATE TABLE `stt_magic_set` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `stt_user` (
-  `usr_uid` int(11) NOT NULL DEFAULT '0',
   `usr_first_name` varchar(50) DEFAULT NULL,
   `usr_last_name` varchar(50) DEFAULT NULL,
   `usr_email` varchar(255) DEFAULT NULL,
@@ -55,16 +55,17 @@ CREATE TABLE `stt_user` (
   `usr_city` varchar(100) DEFAULT NULL,
   `usr_state` varchar(2) DEFAULT NULL,
   `usr_zip` varchar(5) DEFAULT NULL,
-  `usr_phone` varchar(10) DEFAULT NULL,
-  `usr_newsletter` boolean DEFAULT false,
-  `usr_active` boolean DEFAULT true,
+  `usr_phone` varchar(14) DEFAULT NULL,
+  `usr_newsletter` tinyint(1) DEFAULT '0',
+  `usr_active` tinyint(1) DEFAULT '0',
   `usr_dci_number` varchar(10) DEFAULT NULL,
+  `usr_uid` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`usr_uid`),
   UNIQUE KEY `email` (`usr_email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `stt_yugioh_card` (
-  `ycr_uid` int(10) NOT NULL DEFAULT '0',
+  `ycr_uid` int(11) NOT NULL AUTO_INCREMENT,
   `ycr_name` varchar(255) DEFAULT NULL,
   `ycr_card_type` varchar(50) DEFAULT NULL,
   `ycr_attribute` varchar(25) DEFAULT NULL,
@@ -74,141 +75,178 @@ CREATE TABLE `stt_yugioh_card` (
   `ycr_def` int(11) DEFAULT NULL,
   `ycr_description` text,
   PRIMARY KEY (`ycr_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=23778 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stt_email_template` (
+  `etm_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `etm_name` varchar(100) NOT NULL,
+  `etm_body` text NOT NULL,
+  PRIMARY KEY (`etm_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stt_email_variable_group` (
+  `evg_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `evg_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`evg_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stt_email` (
+  `eml_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `eml_name` varchar(100) NOT NULL,
+  `eml_subject` varchar(250) NOT NULL,
+  `eml_from` varchar(1000) NOT NULL,
+  `eml_to` varchar(1000) NOT NULL,
+  `eml_body` text NOT NULL,
+  `eml_etm_uid` int(11) NOT NULL,
+  `eml_evg_uid` int(11) NOT NULL,
+  PRIMARY KEY (`eml_uid`),
+  KEY `eml_etm_uid_fk` (`eml_etm_uid`),
+  KEY `eml_evg_uid_fk` (`eml_evg_uid`),
+  CONSTRAINT `eml_etm_uid_fk` FOREIGN KEY (`eml_etm_uid`) REFERENCES `stt_email_template` (`etm_uid`),
+  CONSTRAINT `eml_evg_uid_fk` FOREIGN KEY (`eml_evg_uid`) REFERENCES `stt_email_variable_group` (`evg_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stt_workflow_event` (
+  `wev_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `wev_name` varchar(100) NOT NULL,
+  `wev_description` varchar(1000) DEFAULT NULL,
+  `wev_code` varchar(100) NOT NULL,
+  PRIMARY KEY (`wev_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stt_email_workflow_event_link` (
+  `ewe_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `ewe_eml_uid` int(11) NOT NULL,
+  `ewe_wev_uid` int(11) NOT NULL,
+  PRIMARY KEY (`ewe_uid`),
+  KEY `ewe_eml_uid_fk` (`ewe_eml_uid`),
+  KEY `ewe_wev_uid_fk` (`ewe_wev_uid`),
+  CONSTRAINT `ewe_eml_uid_fk` FOREIGN KEY (`ewe_eml_uid`) REFERENCES `stt_email` (`eml_uid`),
+  CONSTRAINT `ewe_wev_uid_fk` FOREIGN KEY (`ewe_wev_uid`) REFERENCES `stt_workflow_event` (`wev_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `stt_group_event_link` (
+  `gel_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `gel_evg_uid` int(11) NOT NULL,
+  `gel_wev_uid` int(11) NOT NULL,
+  PRIMARY KEY (`gel_uid`),
+  KEY `gel_evg_uid_fk` (`gel_evg_uid`),
+  KEY `gel_wev_uif_fk` (`gel_wev_uid`),
+  CONSTRAINT `gel_evg_uid_fk` FOREIGN KEY (`gel_evg_uid`) REFERENCES `stt_email_variable_group` (`evg_uid`),
+  CONSTRAINT `gel_wev_uif_fk` FOREIGN KEY (`gel_wev_uid`) REFERENCES `stt_workflow_event` (`wev_uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table stt_email_template  (
-  etm_uid int primary key auto_increment,
-  etm_name varchar(100) not null,
-  etm_body text not null
-);
+CREATE TABLE `stt_email_variable` (
+  `evr_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `evr_evg_uid` int(11) NOT NULL,
+  `evr_name` varchar(50) NOT NULL,
+  `err_default_value` varchar(1000) DEFAULT NULL,
+  `evr_format` varchar(100) DEFAULT NULL,
+  `evr_display_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`evr_uid`),
+  KEY `evr_evg_uid_fk` (`evr_evg_uid`),
+  CONSTRAINT `evr_evg_uid_fk` FOREIGN KEY (`evr_evg_uid`) REFERENCES `stt_email_variable_group` (`evg_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-create table stt_email_variable_group  (
-  evg_uid int primary key auto_increment,
-  evg_name varchar(50)
-);
+CREATE TABLE `stt_webpage` (
+  `wbp_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `wbp_name` varchar(150) DEFAULT NULL,
+  `wbp_body` text,
+  `wbp_code` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`wbp_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-create table stt_email  (
-  eml_uid int primary key auto_increment,
-  eml_name varchar(100) not null,
-  eml_subject varchar(250) not null,
-  eml_from varchar(1000) not null,
-  eml_to varchar(1000) not null,
-  eml_body text not null,
-  eml_etm_uid int not null,
-  constraint eml_etm_uid_fk foreign key (eml_etm_uid) references stt_email_template(etm_uid),
-  eml_evg_uid int not null,
-  constraint eml_evg_uid_fk foreign key (eml_evg_uid) references stt_email_variable_group(evg_uid)
-);
+CREATE TABLE `stt_workflow_event` (
+  `wev_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `wev_name` varchar(100) NOT NULL,
+  `wev_description` varchar(1000) DEFAULT NULL,
+  `wev_code` varchar(100) NOT NULL,
+  PRIMARY KEY (`wev_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-create table stt_workflow_event  (
-  wev_uid int primary key auto_increment,
-  wev_name varchar(100) not null,
-  wev_description varchar(1000),
-  wev_code varchar(100) not null
-);
+CREATE TABLE `stt_order` (
+  `ord_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `ord_usr_uid` int(11) NOT NULL,
+  `ord_status` enum('PLACED','SHIPPED','RETURNED','READY_FOR_PICKUP','CANCELLED','DELIVERED') DEFAULT NULL,
+  `ord_shipping_address1` varchar(150) DEFAULT NULL,
+  `ord_shipping_address2` varchar(100) DEFAULT NULL,
+  `ord_shipping_city` varchar(100) DEFAULT NULL,
+  `ord_shipping_state` varchar(2) DEFAULT NULL,
+  `ord_shipping_zip` varchar(5) DEFAULT NULL,
+  `ord_transaction_id` varchar(10) DEFAULT NULL,
+  `ord_tracking_number` varchar(35) DEFAULT NULL,
+  `ord_in_store_pickup` tinyint(1) DEFAULT NULL,
+  `ord_paid` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`ord_uid`),
+  KEY `ord_usr_uid_fk` (`ord_usr_uid`),
+  CONSTRAINT `ord_usr_uid_fk` FOREIGN KEY (`ord_usr_uid`) REFERENCES `stt_user` (`usr_uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table stt_email_workflow_event_link  (
-  ewe_uid int primary key auto_increment,
-  ewe_eml_uid int not null,
-  constraint ewe_eml_uid_fk foreign key (ewe_eml_uid) references stt_email (eml_uid),
-  ewe_wev_uid int not null,
-  constraint ewe_wev_uid_fk foreign key (ewe_wev_uid) references stt_workflow_event (wev_uid)
-) ;
+CREATE TABLE `stt_event` (
+  `evn_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `evn_date` datetime NOT NULL,
+  `evn_title` varchar(200) DEFAULT NULL,
+  `evn_description` text,
+  `evn_player_limit` int(11) DEFAULT NULL,
+  `evn_number_in_store_users` int(11) DEFAULT NULL,
+  PRIMARY KEY (`evn_uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table stt_group_event_link  (
-  gel_uid int primary key auto_increment,
-  gel_evg_uid int not null,
-  constraint gel_evg_uid_fk foreign key (gel_evg_uid) references stt_email_variable_group (evg_uid),
-  gel_wev_uid int not null,
-  constraint gel_wev_uif_fk foreign key (gel_wev_uid) references stt_workflow_event (wev_uid)
-) ;
+CREATE TABLE `stt_inventory` (
+  `inv_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `inv_price` double DEFAULT NULL,
+  `inv_number_in_stock` int(11) DEFAULT NULL,
+  `inv_name` varchar(200) DEFAULT NULL,
+  `inv_image` varchar(300) DEFAULT NULL,
+  `inv_mcr_uid` int(11) DEFAULT NULL,
+  `inv_ycr_uid` int(11) DEFAULT NULL,
+  `inv_evn_uid` int(11) DEFAULT NULL,
+  `inv_pre_order` tinyint(1) DEFAULT NULL,
+  `inv_description` text,
+  PRIMARY KEY (`inv_uid`),
+  KEY `inv_mcr_uid_fk` (`inv_mcr_uid`),
+  KEY `inv_ycr_uid_fk` (`inv_ycr_uid`),
+  KEY `inv_evn_uid_fk` (`inv_evn_uid`),
+  CONSTRAINT `inv_mcr_uid_fk` FOREIGN KEY (`inv_mcr_uid`) REFERENCES `stt_magic_card` (`mcr_uid`),
+  CONSTRAINT `inv_ycr_uid_fk` FOREIGN KEY (`inv_ycr_uid`) REFERENCES `stt_yugioh_card` (`ycr_uid`),
+  CONSTRAINT `inv_evn_uid_fk` FOREIGN KEY (`inv_evn_uid`) REFERENCES `stt_event` (`evn_uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table stt_email_variable  (
-  evr_uid int primary key auto_increment,
-  evr_evg_uid int not null,
-  constraint evr_evg_uid_fk foreign key (evr_evg_uid) references stt_email_variable_group (evg_uid),
-  evr_name varchar(50) not null,
-  err_default_value varchar(1000),
-  evr_format varchar(100),
-  evr_display_name varchar(100) not null
-);
+CREATE TABLE `stt_inventory_item` (
+  `iit_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `iit_ord_uid` int(11) NOT NULL,
+  `iit_price` double NOT NULL,
+  `iit_name` varchar(200) NOT NULL,
+  `iit_image` varchar(300) NOT NULL,
+  `iit_mcr_uid` int(11) DEFAULT NULL,
+  `iit_ycr_uid` int(11) DEFAULT NULL,
+  `iit_evn_uid` int(11) DEFAULT NULL,
+  `iit_pre_order` tinyint(1) DEFAULT NULL,
+  `iit_description` text NOT NULL,
+  PRIMARY KEY (`iit_uid`),
+  KEY `iit_ord_uid_fk` (`iit_ord_uid`),
+  KEY `iit_mcr_uid_fk` (`iit_mcr_uid`),
+  KEY `iit_ycr_uid_fk` (`iit_ycr_uid`),
+  KEY `iit_evn_uid_fk` (`iit_evn_uid`),
+  CONSTRAINT `iit_ord_uid_fk` FOREIGN KEY (`iit_ord_uid`) REFERENCES `stt_order` (`ord_uid`),
+  CONSTRAINT `iit_mcr_uid_fk` FOREIGN KEY (`iit_mcr_uid`) REFERENCES `stt_inventory` (`inv_uid`),
+  CONSTRAINT `iit_ycr_uid_fk` FOREIGN KEY (`iit_ycr_uid`) REFERENCES `stt_yugioh_card` (`ycr_uid`),
+  CONSTRAINT `iit_evn_uid_fk` FOREIGN KEY (`iit_evn_uid`) REFERENCES `stt_event` (`evn_uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table stt_webpage  (
-  wbp_uid int primary key auto_increment,
-  wbp_name varchar(150),
-  wbp_body text,
-  wbp_code varchar(150)
-);
+CREATE TABLE `stt_stock_notification` (
+  `snt_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `snt_email` varchar(200) DEFAULT NULL,
+  `snt_inv_uid` int(11) NOT NULL,
+  PRIMARY KEY (`snt_uid`),
+  KEY `snt_inv_uid_fk` (`snt_inv_uid`),
+  CONSTRAINT `snt_inv_uid_fk` FOREIGN KEY (`snt_inv_uid`) REFERENCES `stt_inventory` (`inv_uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table stt_announcement  (
-  ann_uid int primary key auto_increment,
-  ann_created datetime,
-  ann_title varchar(150),
-  ann_content text
-);
-
-create table stt_order  (
-  ord_uid int primary key auto_increment,
-  ord_usr_uid int not null,
-  constraint ord_usr_uid_fk foreign key (ord_usr_uid) references stt_user (usr_uid),
-  ord_status enum('PLACED','SHIPPED','RETURNED','READY_FOR_PICKUP', 'CANCELLED', 'DELIVERED') DEFAULT NULL,
-  ord_shipping_address1 varchar(150), -- all shipping address fields nullable in case they are picking it up in the store
-  ord_shipping_address2 varchar(100),
-  ord_shipping_city varchar(100),
-  ord_shipping_state varchar(2),
-  ord_shipping_zip varchar(5),
-  ord_transaction_id varchar(10),
-  ord_tracking_number varchar(35),
-  ord_in_store_pickup tinyint(1),
-  ord_paid tinyint(1)
-);
-
-create table stt_event  (
-  evn_uid int primary key auto_increment,
-  evn_date datetime not null,
-  evn_title varchar(200),
-  evn_description text,
-  evn_player_limit int,
-  evn_number_in_store_users int
-);
-
-create table stt_inventory  (
-  inv_uid int primary key auto_increment,
-  inv_price double,
-  inv_number_in_stock int,
-  inv_name varchar(200),
-  inv_image varchar(300),
-  inv_mcr_uid int,
-  constraint inv_mcr_uid_fk foreign key (inv_mcr_uid) references stt_magic_card (mcr_uid),
-  inv_ycr_uid int,
-  constraint inv_ycr_uid_fk foreign key (inv_ycr_uid) references stt_yugioh_card (ycr_uid),
-  inv_evn_uid int,
-  constraint inv_evn_uid_fk foreign key (inv_evn_uid) references stt_event (evn_uid),
-  inv_pre_order tinyint(1),
-  inv_description text
-);
-
-create table stt_inventory_item  (
-  iit_uid int primary key auto_increment,
-  iit_ord_uid int not null,
-  constraint iit_ord_uid_fk foreign key (iit_ord_uid) references stt_order (ord_uid),
-  iit_price double not null,
-  iit_name varchar(200) not null,
-  iit_image varchar(300) not null,
-  iit_mcr_uid int,
-  constraint iit_mcr_uid_fk foreign key (iit_mcr_uid) references  stt_inventory (inv_uid),
-  iit_ycr_uid int,
-  constraint iit_ycr_uid_fk foreign key (iit_ycr_uid) references stt_yugioh_card (ycr_uid),
-  iit_evn_uid int,
-  constraint iit_evn_uid_fk foreign key (iit_evn_uid) references stt_event (evn_uid),
-  iit_pre_order tinyint(1),
-  iit_description text not null
-);
-
-create table stt_stock_notification  (
-  snt_uid int primary key auto_increment,
-  snt_email varchar(200),
-  snt_inv_uid int not null,
-  constraint snt_inv_uid_fk foreign key (snt_inv_uid) references stt_inventory (inv_uid)
-);
-
+CREATE TABLE `stt_announcement` (
+  `ann_uid` int(11) NOT NULL AUTO_INCREMENT,
+  `ann_created` datetime DEFAULT NULL,
+  `ann_title` varchar(150) DEFAULT NULL,
+  `ann_content` text,
+  PRIMARY KEY (`ann_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
