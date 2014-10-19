207cd69fa32748561abd15165163622a3d786af2
motox683 <motox683@localhost>
2014-10-19 01:08:28 -0700

    Updated indexToQuantity map in the shopping cart session object and added some more front-end work on the shopping cart page.

--- src/com/statboost/models/session/ShoppingCartSessionObject.java
+++ src/com/statboost/models/session/ShoppingCartSessionObject.java
@@ -1,28 +1,35 @@
 package com.statboost.models.session;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
 /**
  * Created by Sam Kerr on 10/18/2014.
+ * This class holds a collection of inventory uid's to reference product inventory from the database
+ * when creating inventory items for an order. This class also maintains a map of cart item indexes
+ * to the quantity of those items wanted by the user.
  */
 public class ShoppingCartSessionObject {
 
     private List<Integer> cartItems;
-    private Map<Integer, Integer> indexToQuantity; //TODO: maintain map of cart item indexes to the quantity of items wanted by the user
+    private Map<Integer, Integer> indexToQuantity;
 
     public ShoppingCartSessionObject() {
         cartItems = new Vector<>();
+        indexToQuantity = new HashMap<>();
     }
 
     /**
      * Add a inventory uid to the shopping cart.  The list of inventory uids will
      * be used to create the collection of inventory items pertaining to the customer's order.
-     * @param itemUID
+     * @param itemUID - uid of product inventory from database
+     * @param quantity - number of items requested by user
      */
-    public void addCartItem(Integer itemUID) {
+    public void addCartItem(Integer itemUID, int quantity) {
         cartItems.add(itemUID);
+        indexToQuantity.put(cartItems.indexOf(itemUID), quantity);
     }
 
     /**
@@ -31,6 +38,7 @@ public class ShoppingCartSessionObject {
      */
     public void removeCartItem(int itemIndex) {
         cartItems.remove(itemIndex);
+        indexToQuantity.remove(itemIndex);
     }
 
     public List<Integer> getCartItems() {
